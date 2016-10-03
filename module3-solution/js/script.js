ce8959645fcbb78c514a29b609384e847dbe489b
Askhat Tynbayev <askhat.tynbayev@gmail.com>
2016-10-03 17:13:51 +0600

    Ready solution to Assignment 3

--- module3-solution/js/script.js
+++
@@ -1,5 +0,0 @@
-$('li.dropdown').on({
-			"shown.bs.dropdown": function() { this.closable = false; $('.container').css('margin-top', '120px'); },
-			"click": function() { this.closable = true; $('.container').css('margin-top', '0px');},
-			"hide.bs.dropdown": function() { return this.closable; $('.container').css('margin-top', '0px');}
-		});
